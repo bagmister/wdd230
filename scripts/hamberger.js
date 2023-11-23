0ae0ea2452183e9b221fb9ead579771bbe83bbf4
kevin bagley <kevin.bagley@progleasing.com>
2023-11-23 00:07:52 -0700

    updates

--- scripts/hamberger.js
+++ scripts/hamberger.js
@@ -1,5 +1,9 @@
 const hamButton = document.querySelector('.hamburger');
-const navigation = document.querySelector('.navigation');
+const navigation = document.querySelector('.nav');
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menuItem");
+const closeIcon= document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
 
 hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
