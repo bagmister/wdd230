aca43152f65ef7a9619f1d2a53f91800ebeb5d97
kevin bagley <bagmister@yahoo.com>
2025-03-13 20:51:02 -0600

    updates

--- scripts/hamberger.js
+++ scripts/hamberger.js
@@ -1,11 +1,22 @@
-const hamButton = document.querySelector('.hamburger');
-const navigation = document.querySelector('.nav');
-const menu = document.querySelector(".menu");
-const menuItems = document.querySelectorAll(".menuItem");
-const closeIcon= document.querySelector(".closeIcon");
-const menuIcon = document.querySelector(".menuIcon");
+const hamburgerMenu = document.querySelector('.hamburger')
+const offScreenMenu = document.querySelector('.off-screen-menu')
+const navigationMenu = document.querySelector('nav')
+const oldTemples = document.querySelector('#siteplan')
+const newTemples = document.querySelector('#chamber')
+const homeTemples = document.querySelector('#home')
+const largeTemples = document.querySelector('#form')
 
-hamButton.addEventListener('click', () => {
-	navigation.classList.toggle('open');
-	hamButton.classList.toggle('open');
-});
+hamburgerMenu.addEventListener('click', () => {
+	hamburgerMenu.classList.toggle('active');
+	offScreenMenu.classList.toggle('active');
+  });
+  
+  navigationMenu.addEventListener('click', (event) => {
+	const navMenuText = event.target.title;
+	galleryHeader.textContent = navMenuText;
+  });
+  
+  offScreenMenu.addEventListener('click', (event) => {
+	const navMenuText = event.target.title;
+	galleryHeader.textContent = navMenuText;
+  });
