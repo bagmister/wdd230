89a6a049d6f7fc380405f521610ca422c4be31ab
kevin bagley <bagmister@yahoo.com>
2025-03-19 23:29:31 -0600

    updates

---
+++ week02/scripts/scripts.js
@@ -0,0 +1,25 @@
+const input = document.querySelector("#favchap")
+const button = document.querySelector("button")
+const list = document.querySelector("#list")
+const li = document.createElement("li")
+const deleteButton = document.createElement("button")
+input.value = '';
+
+function addChapter(input){
+  if (input.value.trim() !== null) {
+    li.textContent = input.value;
+    deleteButton.textContent = '❌';
+    li.append(deleteButton);
+    list.append(li);
+  } else{
+    return input.focus();
+  }
+}
+
+function deleteChapter(li){
+  list.removeChild(li);
+  input.focus();
+}
+
+button.addEventListener('click', addChapter(input))
+deleteButton.addEventListener('click', deleteChapter(li))
