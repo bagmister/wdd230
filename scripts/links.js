67db2486f34f9414056277db0766d241bc3aca87
kevin bagley <bagmister@yahoo.com>
2025-04-13 16:48:22 -0600

    updates

--- scripts/links.js
+++ scripts/links.js
@@ -1,42 +1,53 @@
-const baseURL = "https://bagmister.github.io/wdd230/"
-const linksURL = "https://bagmister.github.io/wdd230/data/links.json"
-const learningActivites = document.querySelector("#learningActivities")
-
+const baseURL = "https://bagmister.github.io/wdd230/";
+const linksURL = "https://bagmister.github.io/wdd230/data/links.json";
+const learningActivities = document.querySelector("#learningActivities");
 
 async function getLinks() {
     try {
-        const response = await fetch(linksURL)
-        if (response.ok){
-            const data = await response.json()
-            displayLinks(data)
-            console.log(data)
+        const response = await fetch(linksURL);
+        if (response.ok) {
+            const data = await response.json();
+            displayLinks(data);
+            console.table(data)
         } else {
-            throw Error(await response.text())
+            throw Error(await response.text());
         }
-
     } catch (error) {
-        console.log(error)
+        console.error(error);
     }
 }
 
-function displayLinks(weeks) {
-    let weeksRefined = weeks.weeks
-    weeksRefined.forEach(week => {
-        let title = document.createElement("h2")
-        let week = document.createElement("")
+function displayLinks(data) {
+    const div = document.createElement("div");
+    div.id = "learningActivities";
 
-        fullName.textContent = `${week.name} ${week.lastname}`;
+    const h2 = document.createElement("h2");
+    h2.textContent = "Learning Activities";
+    div.appendChild(h2);
 
-        portrait.setAttribute('src', week.url);
-        portrait.setAttribute('alt', `Portrait of ${week.name} ${week.lastname}`);
-        portrait.setAttribute('loading', 'lazy');
-        portrait.setAttribute('width', '340');
-        portrait.setAttribute('height', '420');
-        card.appendChild(fullName);
-        card.appendChild(portrait);
+    const ul = document.createElement("ul");
 
-        cards.appendChild(card)
+    data.weeks.forEach((week) => {
+        const li = document.createElement("li");
+        li.textContent = `${week.week} | `
+
+        week.links.forEach((link, index) => {
+            const a = document.createElement("a");
+            const href = link.url.startsWith("http") ? link.url : `${baseURL}${link.url}`;
+            a.setAttribute("href", href);
+            a.textContent = link.title;
+            li.appendChild(a);
+
+            if (index < week.links.length - 1) {
+                li.appendChild(document.createTextNode(" "));
+            }
+        });
+
+        ul.appendChild(li);
     });
+
+    div.appendChild(ul);
+    learningActivities.replaceWith(div);
 }
 
-getLinks()
+getLinks();
